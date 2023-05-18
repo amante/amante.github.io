67094b839183528f897b52979ba12af7870e5909
amante <alexis.mante@gmail.com>
2023-05-18 14:59:54 -0400

    add event
    Testing adding an event to calendar

--- Scripts/ScheduleEvent.js
+++ Scripts/ScheduleEvent.js
@@ -8,11 +8,11 @@ function initEvent(){
     'location': '800 Howard St., San Francisco, CA 94103',
     'description': 'A chance to hear more about Google\'s developer products.',
     'start': {
-      'dateTime': '2015-05-28T09:00:00-07:00',
+      'dateTime': '2023-05-28T09:00:00-07:00',
       'timeZone': 'America/Los_Angeles'
     },
     'end': {
-      'dateTime': '2015-05-28T17:00:00-07:00',
+      'dateTime': '2023-05-28T09-05-28T17:00:00-07:00',
       'timeZone': 'America/Los_Angeles'
     },
     'recurrence': [
