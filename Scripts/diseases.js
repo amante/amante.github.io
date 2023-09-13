3a2d54eca39d1b8c7b859e459e87f9b9dd809324
amante <alexis.mante@gmail.com>
2023-09-13 15:21:16 -0300

    Update diseases.js
    add vaccines !

--- Scripts/diseases.js
+++ Scripts/diseases.js
@@ -1,12 +1,15 @@
 
   document.getElementById("dropdown").innerHTML = `
-  <div>
-  <label for="VaxName">EScoge tu Vacuna</label> 
-    <select name="dog-names" id="dog-names"> 
-        <option value="rigatoni">Rigatoni</option> 
-        <option value="dave">Dave</option> 
-        <option value="pumpernickel">Pumpernickel</option> 
+  <label for="VaxName">Escoge tu Vacuna</label> 
+    <select name="Vacuna" id="Vax-Name"> 
+        <option value="HepatitisB">Hepatitis B</option> 
+        <option value="Hexavalente1">Hexavalente Dosis 1</option>
+        <option value="Hexavalente2">Hexavalente Dosis 2</option>
+        <option value="Hexavalente3">Hexavalente Dosis 3</option>
+        <option value="Hexavalente4">Hexavalente Dosis 4</option>
+        <option value="Neumococica1">Neumocócica Conjugada Dosis 1</option>
+        <option value="Neumococica2">Neumocócica Conjugada Dosis 2</option>
+        <option value="Neumococica3">Neumocócica Conjugada Dosis 3/option>
         <option value="reeses">Reeses</option> 
     </select>
-</div>
 `;
