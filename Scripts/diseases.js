c8c04417f5057dff33b85411e2492245f19f5a62
amante <alexis.mante@gmail.com>
2023-09-13 17:06:43 -0300

    Add diseases and css
    add css and diseases

--- Scripts/diseases.js
+++ Scripts/diseases.js
@@ -1,6 +1,6 @@
 
   document.getElementById("dropdown").innerHTML = `
-  <label for="VaxName">Escoge tu Vacuna</label> 
+  <label for="VaxName" class="dropdown">Escoge tu Vacuna</label> 
     <select name="Vacuna" id="Vax-Name"> 
         <option value="HepatitisB">Hepatitis B</option> 
         <option value="Hexavalente1">Hexavalente Dosis 1</option>
@@ -9,7 +9,17 @@
         <option value="Hexavalente4">Hexavalente Dosis 4</option>
         <option value="Neumococica1">Neumocócica Conjugada Dosis 1</option>
         <option value="Neumococica2">Neumocócica Conjugada Dosis 2</option>
-        <option value="Neumococica3">Neumocócica Conjugada Dosis 3/option>
-        <option value="reeses">Reeses</option> 
+        <option value="Neumococica3">Neumocócica Conjugada Dosis 3</option>
+        <option value="Rotavirus1">Rotavirus Dosis 1</option>
+        <option value="Rotavirus2">Rotavirus Dosis 2</option>
+        <option value="Rotavirus3">Rotavirus Dosis 3</option>
+        <option value="Meningoco1">Meningocócica conjugada ACWY  Dosis 1</option>
+        <option value="Meningoco2">Meningocócica conjugada ACWY  Dosis 1</option>
+        <option value="Meningoco3">Meningocócica conjugada ACWY  Dosis 1</option>
+        <option value="Meningob1">Meningococo B Dosis 1</option>
+        <option value="Meningob2">Meningococo B Dosis 2</option>
+        <option value="Meningob3">Meningococo B Dosis 3</option>
+        <option value="Virica1">Tres Virica Dosis 1</option>
+        <option value="Virica2">Tres Virica Dosis 2</option>
     </select>
 `;
