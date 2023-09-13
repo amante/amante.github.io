645380d6fc87e67e11e4f1200e8733c489905499
amante <alexis.mante@gmail.com>
2023-09-13 12:55:11 -0300

    Test Dropdown
    add dropdown and scripts

--- Scripts/diseases.js
+++ Scripts/diseases.js
@@ -1,3 +1,13 @@
 function initEvent(){
-  
+  document.getElementById("dropdown").innerHTML = `
+<div class="vaccines_dd">
+<label for="dog-names">Choose a dog name:</label> 
+    <select name="dog-names" id="dog-names"> 
+        <option value="rigatoni">Rigatoni</option> 
+        <option value="dave">Dave</option> 
+        <option value="pumpernickel">Pumpernickel</option> 
+        <option value="reeses">Reeses</option> 
+    </select
+</div>
+`;
 }
