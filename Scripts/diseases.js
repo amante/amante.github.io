ad47ad301b196adddb1d329241d4edbd819a7c31
amante <alexis.mante@gmail.com>
2023-09-21 18:07:59 -0300

    Update diseases.js

--- Scripts/diseases.js
+++ Scripts/diseases.js
@@ -3,10 +3,5 @@ const select = document.getElementById('select');
 select.addEventListener('change', handleChange(Event));
 
 function handleChange(event) {
-  console.log("event.target.name"); // 👉️ get selected VALUE
-
-  // 👇️ get selected VALUE even outside event handler
-  console.log(select.options[select.selectedIndex].value);
-
-  // 👇️ get selected TEXT in or outside event handler
-  console.log(select.options[select.selectedIndex].text)};
+  console.log(event.target.name);
+};
