a6f267d09c47af47bcd5c3bc5e61f5fc91b55daa
amante <alexis.mante@gmail.com>
2023-09-21 18:14:56 -0300

    Update diseases.js

--- Scripts/diseases.js
+++ Scripts/diseases.js
@@ -3,5 +3,5 @@ const select = document.getElementById('select');
 select.addEventListener('change', handleChange(Event));
 
 function handleChange(event) {
-  console.log(event.target.name);
+  console.log(event);
 };
