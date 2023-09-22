554c2754c465afd2851d26866526743bcb1bed0b
amante <alexis.mante@gmail.com>
2023-09-22 01:27:08 -0300

    Update diseases.js

--- Scripts/diseases.js
+++ Scripts/diseases.js
@@ -3,6 +3,7 @@ const select = document.getElementById('select');
 function ShowRightMessage(indice){
   switch(indice){
     case 0:
+      document.getElementById("SelectedVaccine").style.display = "none";
       document.getElementById("Messages").style.display = "none";
       document.getElementById("Recordarme").style.display = "none";
   case 1:
@@ -23,7 +24,8 @@ function ShowRightMessage(indice){
 select.addEventListener('change', function handleChange(event) {
   if (event.target.selectedIndex !== 0){
     console.log(event.target.selectedIndex);
-    document.getElementById("SelectedVaccine").innerHTML = `<label> has seleccionado la vacuna = ${event.target.text}</label>`;
+    document.getElementById("SelectedVaccine").style.display = "block";
+    document.getElementById("SelectedVaccine").innerHTML = `<label> has seleccionado la vacuna = ${event.options[selectedIndex].text}</label>`;
     ShowRightMessage(select.selectedIndex);
   }
 });
