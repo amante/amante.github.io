1be80a4a9bf163eb477c05fb15075f3c9087ca51
amante <alexis.mante@gmail.com>
2023-09-22 01:46:03 -0300

    Update diseases.js

--- Scripts/diseases.js
+++ Scripts/diseases.js
@@ -5,14 +5,16 @@ function ShowRightMessage(indice){
     case 0:
       document.getElementById("SelectedVaccine").style.display = "none";
       document.getElementById("Messages").style.display = "none";
-      document.getElementById("Recordarme").style.display = "none";
+      document.getElementById("Recordarme").style.display = "none"; 
   case 1:
+    document.getElementById("SelectedVaccine").style.display = "block";
     document.getElementById("Messages").style.display = "block";
     document.getElementById("Recordarme").style.display = "block";
     document.getElementById("Messages").innerHTML = `<p>¿Quieres que te recordemos tu proxima Vacuna?, deberás agendarte para 1 mes mas</p>`;
     document.getElementById("Recordarme").innerHTML = `<button type="button">Recordarme</button>`; 
     break;
    case 2:
+    document.getElementById("SelectedVaccine").style.display = "block";
     document.getElementById("Messages").style.display = "block";
     document.getElementById("Recordarme").style.display = "block";
      document.getElementById("Messages").innerHTML = `<p>¿Quieres que te recordemos tu proxima Vacuna?, deberás agendarte para 4 meses mas</p>`;
@@ -23,9 +25,9 @@ function ShowRightMessage(indice){
 
 select.addEventListener('change', function handleChange(event) {
   if (event.target.selectedIndex !== 0){
-    console.log(event.target.selectedIndex);
-    document.getElementById("SelectedVaccine").style.display = "block";
-    document.getElementById("SelectedVaccine").innerHTML = `<label> has seleccionado la vacuna = ${event.options[event.selectedIndex].text}</label>`;
+    console.log("Event target index" + event.target.selectedIndex);
+    console.log("Select Index" +select.selectedIndex);
+    document.getElementById("SelectedVaccine").innerHTML = `<label> has seleccionado la vacuna = ${event.target.text}</label>`;
     ShowRightMessage(select.selectedIndex);
   }
-});
+}); 
