fc827d45fe4972b4f38a3f68b1cac8fa0ac2828f
amante <alexis.mante@gmail.com>
2023-09-21 23:20:57 -0300

    Update diseases.js

--- Scripts/diseases.js
+++ Scripts/diseases.js
@@ -1,17 +1,19 @@
 const select = document.getElementById('select');
 
+function showRightMessage(indice){
+  switch(indice){
+  case 0:
+    document.getElementById("Messages").innerHTML = `<p>¿Quieres que te recordemos tu proxima Vacuna?, debes agendarte para 1 mes mas</p>`;
+     break;
+   case 1:
+     document.getElementById("Messages").innerHTML = `<p>¿Quieres que te recordemos tu proxima Vacuna?, debes agendarte para 6 meses mas</p>`;
+     break;
+   }
+
 select.addEventListener('change', function handleChange(event) {
   document.getElementById("SelectedVaccine").innerHTML = `<label> has seleccionado la vacuna = ${event.target.value}</label>`;
   ShowRightMessage(select.selectedIndex);
 });
 
-function showRightMessage(indice){
- switch(indice){
- case 0:
-   document.getElementById("Messages").innerHTML = `<p>¿Quieres que te recordemos tu proxima Vacuna?, debes agendarte para 1 mes mas</p>`;
-    break;
-  case 1:
-    document.getElementById("Messages").innerHTML = `<p>¿Quieres que te recordemos tu proxima Vacuna?, debes agendarte para 6 meses mas</p>`;
-    break;
-  }
+
 };
