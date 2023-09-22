d4a5d851e9f9c47be031f1506eab59e570455d46
amante <alexis.mante@gmail.com>
2023-09-22 01:15:10 -0300

    Update diseases.js

--- Scripts/diseases.js
+++ Scripts/diseases.js
@@ -6,10 +6,14 @@ function ShowRightMessage(indice){
       document.getElementById("Messages").style.display = "none";
       document.getElementById("Recordarme").style.display = "none";
   case 1:
+    document.getElementById("Messages").style.display = "block";
+    document.getElementById("Recordarme").style.display = "block";
     document.getElementById("Messages").innerHTML = `<p>¿Quieres que te recordemos tu proxima Vacuna?, deberás agendarte para 1 mes mas</p>`;
     document.getElementById("Recordarme").innerHTML = `<button type="button">Recordarme</button>`; 
     break;
    case 2:
+    document.getElementById("Messages").style.display = "block";
+    document.getElementById("Recordarme").style.display = "block";
      document.getElementById("Messages").innerHTML = `<p>¿Quieres que te recordemos tu proxima Vacuna?, deberás agendarte para 4 meses mas</p>`;
      document.getElementById("Recordarme").innerHTML = `<button type="button">Recordarme</button>`;
      break;
