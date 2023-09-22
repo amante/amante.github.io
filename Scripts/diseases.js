918c358220dd343992e322e81ad8f153c1dc13ce
amante <alexis.mante@gmail.com>
2023-09-22 00:52:35 -0300

    Summary

--- Scripts/diseases.js
+++ Scripts/diseases.js
@@ -14,7 +14,7 @@ function ShowRightMessage(indice){
   };
 
 select.addEventListener('change', function handleChange(event) {
-  if (event.target.selectedIndex =! 0){
+  if (event.target.selectedIndex !== 0){
     console.log(event.target.selectedIndex);
     document.getElementById("SelectedVaccine").innerHTML = `<label> has seleccionado la vacuna = ${event.target.value}</label>`;
     ShowRightMessage(select.selectedIndex);
