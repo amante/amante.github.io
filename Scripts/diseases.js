f54445d13e87bafc272ffababf60896ca8148094
amante <alexis.mante@gmail.com>
2023-09-22 01:09:09 -0300

    Summary
    Fixing style and main html

--- Scripts/diseases.js
+++ Scripts/diseases.js
@@ -2,12 +2,15 @@ const select = document.getElementById('select');
 
 function ShowRightMessage(indice){
   switch(indice){
+    case 0:
+      document.getElementById("Messages").style.display = "none";
+      document.getElementById("Recordarme").style.display = "none";
   case 1:
-    document.getElementById("Messages").innerHTML = `<p>¿Quieres que te recordemos tu proxima Vacuna?, debes agendarte para 1 mes mas</p>`;
+    document.getElementById("Messages").innerHTML = `<p>¿Quieres que te recordemos tu proxima Vacuna?, deberás agendarte para 1 mes mas</p>`;
     document.getElementById("Recordarme").innerHTML = `<button type="button">Recordarme</button>`; 
     break;
    case 2:
-     document.getElementById("Messages").innerHTML = `<p>¿Quieres que te recordemos tu proxima Vacuna?, debes agendarte para 6 meses mas</p>`;
+     document.getElementById("Messages").innerHTML = `<p>¿Quieres que te recordemos tu proxima Vacuna?, deberás agendarte para 4 meses mas</p>`;
      document.getElementById("Recordarme").innerHTML = `<button type="button">Recordarme</button>`;
      break;
    }
@@ -16,7 +19,7 @@ function ShowRightMessage(indice){
 select.addEventListener('change', function handleChange(event) {
   if (event.target.selectedIndex !== 0){
     console.log(event.target.selectedIndex);
-    document.getElementById("SelectedVaccine").innerHTML = `<label> has seleccionado la vacuna = ${event.target.value}</label>`;
+    document.getElementById("SelectedVaccine").innerHTML = `<label> has seleccionado la vacuna = ${event.target.text}</label>`;
     ShowRightMessage(select.selectedIndex);
   }
 });
