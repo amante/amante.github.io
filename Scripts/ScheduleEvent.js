43cb2f7349d9e4780ac83d2017ea79a5611e6a7d
amante <alexis.mante@gmail.com>
2023-10-05 17:14:50 -0300

    ad info to calendar

--- Scripts/ScheduleEvent.js
+++ Scripts/ScheduleEvent.js
@@ -1,127 +1,23 @@
-/* exported gapiLoaded */
-/* exported gisLoaded */
-/* exported handleAuthClick */
-/* exported handleSignoutClick */
-
-// TODO(developer): Set to client ID and API key from the Developer Console
-const CLIENT_ID = '918312040395-rbia11gk2shf8m39ecjehmm6ibv4921v.apps.googleusercontent.com';
-const API_KEY = 'AIzaSyC2ppfKT1VPuFKvA4NJEKAJs2wicGcf9SI';
-
-// Discovery doc URL for APIs used by the quickstart
-const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
-
-let tokenClient;
-let gapiInited = false;
-let gisInited = false;
-
-//document.getElementById('authorize_button').style.visibility = 'hidden';
-//document.getElementById('signout_button').style.visibility = 'hidden';
-
-/**
- * Callback after api.js is loaded.
- */
-function gapiLoaded() {
-  gapi.load('client', initializeGapiClient);
-}
-
-/**
- * Callback after the API client is loaded. Loads the
- * discovery doc to initialize the API.
- */
-async function initializeGapiClient() {
-  await gapi.client.init({
-    apiKey: API_KEY,
-    discoveryDocs: [DISCOVERY_DOC],
-  });
-  gapiInited = true;
-  maybeEnableButtons();
-}
-
-/**
- * Callback after Google Identity Services are loaded.
- */
-function gisLoaded() {
-  tokenClient = google.accounts.oauth2.initTokenClient({
-    client_id: CLIENT_ID,
-    scope: SCOPES,
-    callback: '', // defined later
-  });
-  gisInited = true;
-  maybeEnableButtons();
-}
-
-/**
- * Enables user interaction after all libraries are loaded.
- */
-function maybeEnableButtons() {
-  if (gapiInited && gisInited) {
-    document.getElementById('authorize_button').style.visibility = 'visible';
-  }
-}
-
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick() {
-  tokenClient.callback = async (resp) => {
-    if (resp.error !== undefined) {
-      throw (resp);
-    }
-    document.getElementById('signout_button').style.visibility = 'visible';
-    document.getElementById('authorize_button').innerText = 'Refresh';
-  };
-
-  if (gapi.client.getToken() === null) {
-    // Prompt the user to select a Google Account and ask for consent to share their data
-    // when establishing a new session.
-    tokenClient.requestAccessToken({prompt: 'consent'});
-  } else {
-    // Skip display of account chooser and consent dialog for an existing session.
-    tokenClient.requestAccessToken({prompt: ''});
-  }
-}
-
-/**
- *  Sign out the user upon button click.
- */
-function handleSignoutClick() {
-  const token = gapi.client.getToken();
-  if (token !== null) {
-    google.accounts.oauth2.revoke(token.access_token);
-    gapi.client.setToken('');
-    document.getElementById('content').innerText = '';
-    document.getElementById('authorize_button').innerText = 'Authorize';
-    document.getElementById('signout_button').style.visibility = 'hidden';
-  }
-}
-
-// Refer to the JavaScript quickstart on how to setup the environment:
-// https://developers.google.com/calendar/quickstart/js
-// Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
-// stored credentials.
-function initEvent() {
+function initEvent(email, vacunatorio, vacuna) {
   var event = {
-    'summary': 'No olvides tu próxima dosis ',
-    'location': 'En este centro de vacunación',
-    'description': 'recuerda agendar tu proxima dosis de vacunación',
+    'summary': 'No olvides tu próxima dosis de' + vacuna,
+    'location': 'En el centro de vacunación:' + vacunatorio,
+    'description': 'No olvides agendar tu próxima dosis de vacunación de' + vacuna,
     'start': {
-      'dateTime': '2015-05-28T09:00:00-07:00',
-      'timeZone': 'America/Los_Angeles'
+      'dateTime': '2023-10-28T09:00:00-07:00',
+      'timeZone': 'America/Chile/Santiago'
     },
     'end': {
-      'dateTime': '2015-05-28T17:00:00-07:00',
-      'timeZone': 'America/Los_Angeles'
+      'dateTime': '2023-10-28T17:00:00-07:00',
+      'timeZone': 'America/Chile/Santiago'
     },
     'recurrence': [
       'RRULE:FREQ=DAILY;COUNT=2'
     ],
     'attendees': [
-      {'email': 'alexis.mante@gmail.com'},
-      {'email': 'alexis.manuel.mante@merck.com'}
+
+      {'email': 'monomaldito@hotmail.com'},
+      {'email': email}
     ],
     'reminders': {
       'useDefault': false,
@@ -131,13 +27,12 @@ function initEvent() {
       ]
     }
   };
-  console.log("me estan llamando desde el HTML")
 
   var request = gapi.client.calendar.events.insert({
     'calendarId': 'primary',
     'resource': event
   });
-  console.log("me estan llamando despeus del request")
+
   request.execute(function(event) {
     appendPre('Event created: ' + event.htmlLink);
   });
