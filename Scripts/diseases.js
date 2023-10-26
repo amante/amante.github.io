abcc511ddabfbcf8394b14575483742b61bf7d6e
amante <alexis.mante@gmail.com>
2023-10-26 10:42:18 -0300

    add indice as a var in disease.js

--- Scripts/diseases.js
+++ Scripts/diseases.js
@@ -3,18 +3,20 @@ const select = document.getElementById('select');
 function ShowRightMessage(indice){
   switch(indice){
   case 1:
+    console.log("case 1" + indice);
     document.getElementById("SelectedVaccine").style.display = "block";
     document.getElementById("Messages").style.display = "block";
     document.getElementById("Recordarme").style.display = "block";
     document.getElementById("Messages").innerHTML = `<label class="text">¿Quieres que te recordemos tu proxima Vacuna?, deberás agendarte para 1 mes mas</label>`;
-    document.getElementById("Recordarme").innerHTML = `<button id="recordarme" class="text-center"><a location href='https://amante.github.io/AgendateVPH.html?selection=`indice`'>Recordarme</a></button>`; 
+    document.getElementById("Recordarme").innerHTML = `<button id="recordarme" class="text-center"><a location href='https://amante.github.io/AgendateVPH.html?selection=${indice}'>Recordarme</a></button>`; 
     break;
    case 2:
+    console.log("case 2" + indice);
     document.getElementById("SelectedVaccine").style.display = "block";
     document.getElementById("Messages").style.display = "block";
     document.getElementById("Recordarme").style.display = "block";
     document.getElementById("Messages").innerHTML = `<label class="text">¿Quieres que te recordemos tu proxima Vacuna?, deberás agendarte para 4 meses mas</label>`;
-    document.getElementById("Recordarme").innerHTML = `<button id="recordarme" class="text-center"><a href='https://amante.github.io/AgendateVPH.html?selection=`indice`'>Recordarme</a></button>`;
+    document.getElementById("Recordarme").innerHTML = `<button id="recordarme" class="text-center"><a href='https://amante.github.io/AgendateVPH.html?selection= ${indice}'>Recordarme</a></button>`;
      break;
    }
   };
