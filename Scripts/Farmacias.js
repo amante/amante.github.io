176d6d2242f17aad8d1ebcfb2dec871f840522ce
amante <alexis.mante@gmail.com>
2024-01-23 16:31:02 -0300

    1 marker in map

--- Scripts/Farmacias.js
+++ Scripts/Farmacias.js
@@ -1,7 +1,21 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 12,
+    zoom: 15,
     center: { lat: -33.447487, lng: -70.673676 },
   });
+  
+  function createMarker(place) {
+    if (!place.geometry || !place.geometry.location) return;
+  
+    const marker = new google.maps.Marker({
+      map,
+      position: { lat: -33.522345, lng: -70.579265 },
+    });
+  
+    google.maps.event.addListener(marker, "click", () => {
+      infowindow.setContent(place.name || "");
+      infowindow.open(map);
+    });
+  }
 }
 window.initMap = initMap;
