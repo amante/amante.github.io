61db56104bcd998eda26cc6b449a1391ed73ccf1
amante <alexis.mante@gmail.com>
2024-01-24 17:15:01 -0300

    test

--- Scripts/Farmacias.js
+++ Scripts/Farmacias.js
@@ -26,41 +26,12 @@ function initMap() {
     ];
   
     for (let i = 0; i < Pharmacies.length; i++) {
-      console.log("Arrrglo 1" + Pharmacies.length);
+      console.log("Arreglo 1" + Pharmacies.length);
       for (let j = 0; j < Pharmacies[i].length; j++) {
         console.log("Arreglo 2" + Pharmacies[i].length);
         console.log(Pharmacies[i][j]);
       }
-   
-
-    const infowindow = new google.maps.InfoWindow();
-
-    google.maps.event.addListener(marker, "click", () => {
-    const content = document.createElement("div");
-  
-    const nameElement = document.createElement("h2");
-    nameElement.textContent = "Farmacia Cruz verde";
-    content.appendChild(nameElement);
-  
-    const placeAddressElement = document.createElement("p");
-    placeAddressElement.textContent = "Dirección: Walker Martinez 1786";
-    content.appendChild(placeAddressElement);
-    
-    const comunaElement = document.createElement("p");
-    comunaElement.textContent = "Comuna: La Florida ";
-    content.appendChild(comunaElement);
-  
-    const regionElement = document.createElement("p");
-    regionElement.textContent = "Region: Metroplitana ";
-    content.appendChild(regionElement);
-  
-    const stockElement = document.createElement("p");
-    stockElement.textContent = "Stock de Gardasil 9: 10 unidades ";
-    content.appendChild(stockElement);
-
-    infowindow.setContent(content);
-    infowindow.open(map, marker);
-    });
+    }
   }
-}
+   
 window.initMap = initMap;
