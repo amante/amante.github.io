ee933e255ee62e6c16fbba2cd2568d4efcfa24ef
amante <alexis.mante@gmail.com>
2024-01-24 11:10:17 -0300

    add info to infowindow

--- Scripts/Farmacias.js
+++ Scripts/Farmacias.js
@@ -28,11 +28,29 @@ function initMap() {
   const infowindow = new google.maps.InfoWindow();
 
   google.maps.event.addListener(marker, "click", () => {
+    const content = document.createElement("div");
+  
+    const nameElement = document.createElement("h2");
+    nameElement.textContent = "Farmacia Cruz verde";
+    content.appendChild(nameElement);
+  
+    const placeAddressElement = document.createElement("p");
+    placeAddressElement.textContent = "Dirección: Walker Martinez 1786";
+    content.appendChild(placeAddressElement);
+    
+    const comunaElement = document.createElement("p");
+    comunaElement.textContent = "Comuna: La Florida ";
+    content.appendChild(comunaElement);
+  
+    const regionElement = document.createElement("p");
+    regionElement.textContent = "Region: Metroplitana ";
+    content.appendChild(regionElement);
+  
+    const stockElement = document.createElement("p");
+    stockElement.textContent = "Stock de Gardasil 9: 10 unidades ";
+    content.appendChild(regionElement);
     infowindow.open(map, marker);
   });
 }
 
 window.initMap = initMap;
-   
-
-  
