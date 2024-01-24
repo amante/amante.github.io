31b79ae48deb8c90e96366c2b4ce52be09ee0a03
amante <alexis.mante@gmail.com>
2024-01-24 09:58:39 -0300

    Cargar datos de prueba en infowindow

--- Scripts/Farmacias.js
+++ Scripts/Farmacias.js
@@ -6,30 +6,45 @@ let map;
 
 function initMap() {
   const mapOptions = {
-    zoom: 8,
+    zoom: 15,
     center: { lat: -33.447487, lng: -70.673676 },
   };
 
   map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
   const marker = new google.maps.Marker({
-    // The below line is equivalent to writing:
-    // position: new google.maps.LatLng(-34.397, 150.644)
-    position: { lat: -33.522345, lng: -70.579265 },
+    position: { lat: -33.522345, lng: -70,579265 },
     map: map,
   });
-  // You can use a LatLng literal in place of a google.maps.LatLng object when
-  // creating the Marker object. Once the Marker object is instantiated, its
-  // position will be available as a google.maps.LatLng object. In this case,
-  // we retrieve the marker's position using the
-  // google.maps.LatLng.getPosition() method.
-  const infowindow = new google.maps.InfoWindow({
-    content: "<p>Stock Disponible:" + "10 unidades" + "</p>",
-  });
+  
+    const infowindow = new google.maps.InfoWindow();
 
-  google.maps.event.addListener(marker, "click", () => {
-    infowindow.open(map, marker);
-  });
+    google.maps.event.addListener(marker, "click", () => {
+      const content = document.createElement("div");
+      
+      const nameElement = document.createElement("h2");
+      nameElement.textContent = "Farmacia Cruz verde";
+      content.appendChild(nameElement);
+    
+      const placeAddressElement = document.createElement("p");
+      placeAddressElement.textContent = "Dirección: Walker Martinez 1786";
+      content.appendChild(placeAddressElement);
+      
+      const comunaElement = document.createElement("p");
+      comunaElement.textContent = "Comuna: La Florida ";
+      content.appendChild(comunaElement);
+
+      const regionElement = document.createElement("p");
+      regionElement.textContent = "Region: Metroplitana ";
+      content.appendChild(regionElement);
+
+      const stockElement = document.createElement("p");
+      stockElement.textContent = "Stock de Gardasil 9: 10 unidades ";
+      content.appendChild(regionElement);
+
+      infowindow.setContent(content);
+      infowindow.open(map, marker);
+    });
 }
 
 window.initMap = initMap;
