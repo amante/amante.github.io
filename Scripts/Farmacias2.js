77aa1af0d73c8aa2b21b6e79d3ae126c075063db
amante <alexis.mante@gmail.com>
2024-01-30 15:30:08 -0300

    add array

--- Scripts/Farmacias2.js
+++ Scripts/Farmacias2.js
@@ -6,30 +6,36 @@ let service;
 let infowindow;
 
 function initMap() {
-  const sydney = new google.maps.LatLng(-33.867, 151.195);
+  const santiago = new google.maps.LatLng(-33.447487,-70.673676);
 
   infowindow = new google.maps.InfoWindow();
   map = new google.maps.Map(document.getElementById("map"), {
-    center: sydney,
+    center: santiago,
     zoom: 15,
   });
 
-  const request = {
-    query: "Museum of Contemporary Art Australia",
-    fields: ["name", "geometry"],
-  };
+ // const request = {
+   // query: "Museum of Contemporary Art Australia",
+    //fields: ["name", "geometry"],
+ // };
 
-  service = new google.maps.places.PlacesService(map);
-  service.findPlaceFromQuery(request, (results, status) => {
-    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+  //service = new google.maps.places.PlacesService(map);
+  //service.findPlaceFromQuery(request, (results, status) => {
+   // if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+
+   results = [
+    ['IBERIA', 'VICTORIA Nº 3092', 'VALPARAISO', 'VALPARAISO', -33.0507821994016, -71.6029768890481], 
+    ['BOTICA UNIÓN', 'CONDELL 1205', 'VALPARAISO', 'VALPARAISO', -33.0436526830044, -71.6243601197905], 
+    ['GALENICA', 'ARLEGUI N° 580 L - 7 OF. 201', 'VALPARAISO', 'VIÑA DEL MAR', -33.0236943992421, -71.5542400534807]
+];
       for (let i = 0; i < results.length; i++) {
         createMarker(results[i]);
       }
 
       map.setCenter(results[0].geometry.location);
     }
-  });
-}
+  //});
+//}
 
 function createMarker(place) {
   if (!place.geometry || !place.geometry.location) return;
