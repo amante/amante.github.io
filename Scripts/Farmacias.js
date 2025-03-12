253a4f7b515059d1a6bc579935bb386a0514bd86
amante <alexis.mante@gmail.com>
2025-03-12 12:26:17 -0300

    test code

--- Scripts/Farmacias.js
+++ Scripts/Farmacias.js
@@ -148,14 +148,23 @@ function setMarkers(map) {
 
 window.initMap = initMap();*/
 
-let map;
-
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 async function initMap() {
+  // Request needed libraries.
   const { Map } = await google.maps.importLibrary("maps");
-
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const map = new Map(document.getElementById("map"), {
+    center: { lat: 37.4239163, lng: -122.0947209 },
+    zoom: 14,
+    mapId: "4504f8b37365c3d0",
+  });
+  const marker = new AdvancedMarkerElement({
+    map,
+    position: { lat: 37.4239163, lng: -122.0947209 },
   });
 }
 
