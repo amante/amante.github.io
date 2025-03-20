519d54ddfb8be494a0c7e147d3fe698ec3e69955
amante <alexis.mante@gmail.com>
2025-03-20 03:42:10 -0300

    test results

--- Scripts/FarmaciasDev.js
+++ Scripts/FarmaciasDev.js
@@ -59,7 +59,7 @@ const farmasChile = function(results) {
   };
 
   function FilterMarkerStock() {
-    const farmacias = farmasChile(); 
+    const farmacias = farmasChile(results); 
     const select = document.getElementById("selector").value;
     clearMarkers();
   
